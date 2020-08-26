737af8f44655a612c7816c3122303ac93b0e23c1
heebeers <hieu.b.trinh@gmail.com>
2020-08-26 15:26:40 -0500

    added cross origin to repo

--- CRM/src/main/java/com/ceme/crm/CRM/data/InteractionRepository.java
+++ CRM/src/main/java/com/ceme/crm/CRM/data/InteractionRepository.java
@@ -7,7 +7,9 @@ import com.ceme.crm.CRM.entities.Interaction;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "interactions", path = "interactions")
 public interface InteractionRepository extends MongoRepository<Interaction, String> {
 

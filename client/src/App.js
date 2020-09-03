6127a0cfe6f02409f9b35995905b1c11967da60e
Chad Whitt <me@youhavechad.com>
2020-09-03 11:56:00 -0500

    Adding Clock, etc.

--- client/src/App.js
+++ client/src/App.js
@@ -8,24 +8,14 @@ import CreateInteraction from "./interactions/createInteraction";
 import CustomerList from "./customer/customerList";
 import InteractionList from "./interactions/interactionsList";
 import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+import Clock from "./components/Clock";
 
 function App(props) {
   return (
     <Router>
       <Header />
       <Container className="container" style={{ marginTop: "7em" }}>
-        <Container text>
-          <h1>Placeholder Title</h1>
-          <p>
-            Seitan eu try-hard la croix you probably haven't heard of them
-            locavore. Sriracha cray offal, magna echo park swag dolore mollit
-            pour-over cliche venmo irure scenester. Kickstarter live-edge
-            shoreditch glossier locavore, anim forage adipisicing. Fashion axe
-            schlitz cred roof party kinfolk, kale chips migas dolore kombucha
-            retro gentrify subway tile drinking vinegar paleo.
-          </p>
-        </Container>
-
         <Switch>
           <Route path="/listCustomers" component={CustomerList} />
           <Route path="/listInteractions" component={InteractionList} />
@@ -33,8 +23,9 @@ function App(props) {
           <Route exact path="/edit" component={Edit} />
           <Route exact path="/showInteractions" component={InteractionList} />
           <Route exact path="/addInteraction" component={CreateInteraction} />
-          <Route exact path="/index" component={App} />
+          <Route exact path="/" component={HomePage} />
         </Switch>
+        <Clock />
       </Container>
     </Router>
   );

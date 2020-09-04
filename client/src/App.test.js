3a091658ecf03d0f5b036cc11b55fe8e565f1363
Chad Whitt <me@youhavechad.com>
2020-09-03 20:59:23 -0500

    WIP Jest.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// it("renders without crashing", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(<App />, div);
+// });
+
+// test("renders learn react link", () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/create/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("My Test Suite", () => {
+  it("My Test Case", () => {
+    expect(true).toEqual(true);
+  });
 });

efccd7682faac63211732d73bc84d8580fb5bce1
heebeers <hieu.b.trinh@gmail.com>
2020-09-04 11:04:01 -0500

    update interaction update to utilize interaction constants

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,355 +1,362 @@
 // this is not changed for functional components
 
 import axios from "axios";
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 //import rootReducer from './../reducer/index';
 import customersReducer from "../reducer/customerReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-
-// these constants can be used as the names of the actions 
+// these constants can be used as the names of the actions
 // so you minimise using the wrong string
-export const FETCH_CUSTOMERS_BEGIN   = 'FETCH_CUSTOMERS_BEGIN';
-export const FETCH_CUSTOMERS_SUCCESS = 'FETCH_CUSTOMERS_SUCCESS';
-export const FETCH_CUSTOMERS_FAILURE = 'FETCH_CUSTOMERS_FAILURE';
+export const FETCH_CUSTOMERS_BEGIN = "FETCH_CUSTOMERS_BEGIN";
+export const FETCH_CUSTOMERS_SUCCESS = "FETCH_CUSTOMERS_SUCCESS";
+export const FETCH_CUSTOMERS_FAILURE = "FETCH_CUSTOMERS_FAILURE";
 
-export const GET_CUSTOMER_BEGIN   = 'GET_CUSTOMER_BEGIN';
-export const GET_CUSTOMER_SUCCESS = 'GET_CUSTOMER_SUCCESS';
-export const GET_CUSTOMER_FAILURE = 'GET_CUSTOMER_FAILURE';
+export const GET_CUSTOMER_BEGIN = "GET_CUSTOMER_BEGIN";
+export const GET_CUSTOMER_SUCCESS = "GET_CUSTOMER_SUCCESS";
+export const GET_CUSTOMER_FAILURE = "GET_CUSTOMER_FAILURE";
 
-export const ADD_CUSTOMER_BEGIN   = 'ADD_CUSTOMER_BEGIN';
-export const ADD_CUSTOMER_SUCCESS = 'ADD_CUSTOMER_SUCCESS';
-export const ADD_CUSTOMER_FAILURE = 'ADD_CUSTOMER_FAILURE';
+export const ADD_CUSTOMER_BEGIN = "ADD_CUSTOMER_BEGIN";
+export const ADD_CUSTOMER_SUCCESS = "ADD_CUSTOMER_SUCCESS";
+export const ADD_CUSTOMER_FAILURE = "ADD_CUSTOMER_FAILURE";
 
-export const UPDATE_CUSTOMER_BEGIN   = 'UPDATE_CUSTOMER_BEGIN';
-export const UPDATE_CUSTOMER_SUCCESS = 'UPDATE_CUSTOMER_SUCCESS';
-export const UPDATE_CUSTOMER_FAILURE = 'UPDATE_CUSTOMER_FAILURE';
+export const UPDATE_CUSTOMER_BEGIN = "UPDATE_CUSTOMER_BEGIN";
+export const UPDATE_CUSTOMER_SUCCESS = "UPDATE_CUSTOMER_SUCCESS";
+export const UPDATE_CUSTOMER_FAILURE = "UPDATE_CUSTOMER_FAILURE";
 
-export const DELETE_CUSTOMER_BEGIN   = 'DELETE_CUSTOMER_BEGIN';
-export const DELETE_CUSTOMER_SUCCESS = 'DELETE_CUSTOMER_SUCCESS';
-export const DELETE_CUSTOMER_FAILURE = 'DELETE_CUSTOMER_FAILURE';
+export const DELETE_CUSTOMER_BEGIN = "DELETE_CUSTOMER_BEGIN";
+export const DELETE_CUSTOMER_SUCCESS = "DELETE_CUSTOMER_SUCCESS";
+export const DELETE_CUSTOMER_FAILURE = "DELETE_CUSTOMER_FAILURE";
 
-export const FETCH_INTERACTIONS_BEGIN   = 'FETCH_INTERACTIONS_BEGIN';
-export const FETCH_INTERACTIONS_SUCCESS = 'FETCH_INTERACTIONS_SUCCESS';
-export const FETCH_INTERACTIONS_FAILURE = 'FETCH_INTERACTIONS_FAILURE';
+export const FETCH_INTERACTIONS_BEGIN = "FETCH_INTERACTIONS_BEGIN";
+export const FETCH_INTERACTIONS_SUCCESS = "FETCH_INTERACTIONS_SUCCESS";
+export const FETCH_INTERACTIONS_FAILURE = "FETCH_INTERACTIONS_FAILURE";
 
-export const GET_INTERACTION_BEGIN   = 'GET_INTERACTION_BEGIN';
-export const GET_INTERACTION_SUCCESS = 'GET_INTERACTION_SUCCESS';
-export const GET_INTERACTION_FAILURE = 'GET_INTERACTION_FAILURE';
+export const GET_INTERACTION_BEGIN = "GET_INTERACTION_BEGIN";
+export const GET_INTERACTION_SUCCESS = "GET_INTERACTION_SUCCESS";
+export const GET_INTERACTION_FAILURE = "GET_INTERACTION_FAILURE";
 
-export const ADD_INTERACTION_BEGIN   = 'ADD_INTERACTION_BEGIN';
-export const ADD_INTERACTION_SUCCESS = 'ADD_INTERACTION_SUCCESS';
-export const ADD_INTERACTION_FAILURE = 'ADD_INTERACTION_FAILURE';
+export const ADD_INTERACTION_BEGIN = "ADD_INTERACTION_BEGIN";
+export const ADD_INTERACTION_SUCCESS = "ADD_INTERACTION_SUCCESS";
+export const ADD_INTERACTION_FAILURE = "ADD_INTERACTION_FAILURE";
 
-export const UPDATE_INTERACTION_BEGIN   = 'UPDATE_INTERACTION_BEGIN';
-export const UPDATE_INTERACTION_SUCCESS = 'UPDATE_INTERACTION_SUCCESS';
-export const UPDATE_INTERACTION_FAILURE = 'UPDATE_INTERACTION_FAILURE';
+export const UPDATE_INTERACTION_BEGIN = "UPDATE_INTERACTION_BEGIN";
+export const UPDATE_INTERACTION_SUCCESS = "UPDATE_INTERACTION_SUCCESS";
+export const UPDATE_INTERACTION_FAILURE = "UPDATE_INTERACTION_FAILURE";
 
-export const DELETE_INTERACTION_BEGIN   = 'DELETE_INTERACTION_BEGIN';
-export const DELETE_INTERACTION_SUCCESS = 'DELETE_INTERACTION_SUCCESS';
-export const DELETE_INTERACTION_FAILURE = 'DELETE_INTERACTION_FAILURE';
+export const DELETE_INTERACTION_BEGIN = "DELETE_INTERACTION_BEGIN";
+export const DELETE_INTERACTION_SUCCESS = "DELETE_INTERACTION_SUCCESS";
+export const DELETE_INTERACTION_FAILURE = "DELETE_INTERACTION_FAILURE";
 
 // Add Interaction
 export const addINTERACTIONBegin = () => ({
-  type: ADD_CUSTOMER_BEGIN
+  type: ADD_INTERACTION_BEGIN,
 });
 
-export const addINTERACTIONSuccess = customers => ({
-  type: ADD_CUSTOMER_SUCCESS,
-  payload: { message: "success" }
+export const addINTERACTIONSuccess = (customers) => ({
+  type: ADD_INTERACTION_SUCCESS,
+  payload: { message: "success" },
 });
 
-export const addINTERACTIONFailure = error => ({
-  type: ADD_CUSTOMER_FAILURE,
-  payload: { error }
+export const addINTERACTIONFailure = (error) => ({
+  type: ADD_INTERACTION_FAILURE,
+  payload: { error },
 });
 
 // Get All Interactions
 
 export const getINTERACTIONBegin = () => ({
-  type: GET_INTERACTION_BEGIN
-  
+  type: GET_INTERACTION_BEGIN,
 });
 
-export const getINTERACTIONSuccess = interaction => ({
+export const getINTERACTIONSuccess = (interaction) => ({
   type: GET_INTERACTION_SUCCESS,
-  payload: { interaction }
+  payload: { interaction },
 });
 
-export const getINTERACTIONFailure = error => ({
+export const getINTERACTIONFailure = (error) => ({
   type: GET_INTERACTION_FAILURE,
-  payload: { error }
+  payload: { error },
 });
 
 // Update Interaction
 export const updateINTERACTIONBegin = () => ({
-  type: UPDATE_INTERACTION_BEGIN
+  type: UPDATE_INTERACTION_BEGIN,
 });
 
-export const updateINTERACTIONSuccess = interactions => ({
+export const updateINTERACTIONSuccess = (interactions) => ({
   type: UPDATE_INTERACTION_SUCCESS,
-  payload: { message: "success" }
+  payload: { message: "success" },
 });
 
-export const updateINTERACTIONFailure = error => ({
+export const updateINTERACTIONFailure = (error) => ({
   type: UPDATE_INTERACTION_FAILURE,
-  payload: { error }
+  payload: { error },
 });
 
 // Delete Interaction
 export const deleteINTERACTIONBegin = () => ({
-  type: DELETE_INTERACTION_BEGIN
+  type: DELETE_INTERACTION_BEGIN,
 });
 
-export const deleteINTERACTIONSuccess = interactions => ({
+export const deleteINTERACTIONSuccess = (interactions) => ({
   type: DELETE_INTERACTION_SUCCESS,
-  payload: { message: "success" }
+  payload: { message: "success" },
 });
 
-export const deleteINTERACTIONFailure = error => ({
+export const deleteINTERACTIONFailure = (error) => ({
   type: DELETE_INTERACTION_FAILURE,
-  payload: { error }
+  payload: { error },
 });
 
 // Fetch Interactions by Customer Id
 export const fetchINTERACTIONsBegin = () => ({
-  type: FETCH_INTERACTIONS_BEGIN
-  
+  type: FETCH_INTERACTIONS_BEGIN,
 });
 
-export const fetchINTERACTIONsSuccess = interactions => ({
+export const fetchINTERACTIONsSuccess = (interactions) => ({
   type: FETCH_INTERACTIONS_SUCCESS,
-  payload: { interactions }
+  payload: { interactions },
 });
 
-export const fetchINTERACTIONsFailure = error => ({
+export const fetchINTERACTIONsFailure = (error) => ({
   type: FETCH_INTERACTIONS_FAILURE,
-  payload: { error }
+  payload: { error },
 });
 
 // Fetch Customers
 export const fetchCUSTOMERsBegin = () => ({
-  type: FETCH_CUSTOMERS_BEGIN
-  
+  type: FETCH_CUSTOMERS_BEGIN,
 });
 
-export const fetchCUSTOMERsSuccess = customers => ({
+export const fetchCUSTOMERsSuccess = (customers) => ({
   type: FETCH_CUSTOMERS_SUCCESS,
-  payload: { customers }
+  payload: { customers },
 });
 
-export const fetchCUSTOMERsFailure = error => ({
+export const fetchCUSTOMERsFailure = (error) => ({
   type: FETCH_CUSTOMERS_FAILURE,
-  payload: { error }
+  payload: { error },
 });
 
 //Get Customer
 export const getCUSTOMERBegin = () => ({
-  type: GET_CUSTOMER_BEGIN
-  
+  type: GET_CUSTOMER_BEGIN,
 });
 
-export const getCUSTOMERSuccess = customer => ({
+export const getCUSTOMERSuccess = (customer) => ({
   type: GET_CUSTOMER_SUCCESS,
-  payload: { customer }
+  payload: { customer },
 });
 
-export const getCUSTOMERFailure = error => ({
+export const getCUSTOMERFailure = (error) => ({
   type: GET_CUSTOMER_FAILURE,
-  payload: { error }
+  payload: { error },
 });
 
 // Add CUSTOMER
 export const addCUSTOMERBegin = () => ({
-  type: ADD_CUSTOMER_BEGIN
+  type: ADD_CUSTOMER_BEGIN,
 });
 
-export const addCUSTOMERSuccess = customers => ({
+export const addCUSTOMERSuccess = (customers) => ({
   type: ADD_CUSTOMER_SUCCESS,
-  payload: { message: "success" }
+  payload: { message: "success" },
 });
 
-export const addCUSTOMERFailure = error => ({
+export const addCUSTOMERFailure = (error) => ({
   type: ADD_CUSTOMER_FAILURE,
-  payload: { error }
+  payload: { error },
 });
 
 // Update CUSTOMER
 export const updateCUSTOMERBegin = () => ({
-  type: UPDATE_CUSTOMER_BEGIN
+  type: UPDATE_CUSTOMER_BEGIN,
 });
 
-export const updateCUSTOMERSuccess = customers => ({
+export const updateCUSTOMERSuccess = (customers) => ({
   type: UPDATE_CUSTOMER_SUCCESS,
-  payload: { message: "success" }
+  payload: { message: "success" },
 });
 
-export const updateCUSTOMERFailure = error => ({
+export const updateCUSTOMERFailure = (error) => ({
   type: UPDATE_CUSTOMER_FAILURE,
-  payload: { error }
+  payload: { error },
 });
 
 // Delete CUSTOMER
 export const deleteCUSTOMERBegin = () => ({
-  type: DELETE_CUSTOMER_BEGIN
+  type: DELETE_CUSTOMER_BEGIN,
 });
 
-export const deleteCUSTOMERSuccess = customers => ({
+export const deleteCUSTOMERSuccess = (customers) => ({
   type: DELETE_CUSTOMER_SUCCESS,
-  payload: { message: "success" }
+  payload: { message: "success" },
 });
 
-export const deleteCUSTOMERFailure = error => ({
+export const deleteCUSTOMERFailure = (error) => ({
   type: DELETE_CUSTOMER_FAILURE,
-  payload: { error }
+  payload: { error },
 });
-
-
-
 
 export function fetchCUSTOMERs() {
-  return dispatch => {
+  return (dispatch) => {
     dispatch(fetchCUSTOMERsBegin());
     axios
-    .get("http://localhost:8080/customers")
-    .then(response => {
-      console.log(response.data);
-      dispatch(fetchCUSTOMERsSuccess(response.data._embedded.customers));
-    })
-    .catch(error => dispatch(fetchCUSTOMERsFailure(error)));
-  }
+      .get("http://localhost:8080/customers")
+      .then((response) => {
+        console.log(response.data);
+        dispatch(fetchCUSTOMERsSuccess(response.data._embedded.customers));
+      })
+      .catch((error) => dispatch(fetchCUSTOMERsFailure(error)));
+  };
 }
 
 export function getCUSTOMER(id) {
-  return dispatch => {
+  return (dispatch) => {
     dispatch(getCUSTOMERBegin());
     axios
-    .get("http://localhost:8080/customers/"+ id)
-    .then(response => {
-      console.log(response.data);
-      dispatch(getCUSTOMERSuccess(response.data));
-    })
-    .catch(error => dispatch(getCUSTOMERFailure(error)));
-  }
+      .get("http://localhost:8080/customers/" + id)
+      .then((response) => {
+        console.log(response.data);
+        dispatch(getCUSTOMERSuccess(response.data));
+      })
+      .catch((error) => dispatch(getCUSTOMERFailure(error)));
+  };
 }
 
 export function addCUSTOMER(customer) {
-  return dispatch => {
+  return (dispatch) => {
     dispatch(addCUSTOMERBegin());
     axios
-    .post("http://localhost:8080/customers", customer)
-    .then(response => {
-      console.log(response.data);
-      dispatch(addCUSTOMERSuccess(response.data));
-    })
-    .catch(error => dispatch(addCUSTOMERFailure(error)));
-  }
+      .post("http://localhost:8080/customers", customer)
+      .then((response) => {
+        console.log(response.data);
+        dispatch(addCUSTOMERSuccess(response.data));
+      })
+      .catch((error) => dispatch(addCUSTOMERFailure(error)));
+  };
 }
 
 export function updateCUSTOMER(customer) {
-  return dispatch => {
+  return (dispatch) => {
     dispatch(updateCUSTOMERBegin());
     console.log(customer._links.self.href.substring(32));
     axios
-    //.post("http://localhost:8080/customers", customer)
-    .put("http://localhost:8080/customers/" + customer._links.self.href.substring(32), customer)
-    .then(response => {
-      console.log(response.data);
-      dispatch(updateCUSTOMERSuccess(response.data));
-    })
-    .catch(error => dispatch(updateCUSTOMERFailure(error)));
-  }
+      //.post("http://localhost:8080/customers", customer)
+      .put(
+        "http://localhost:8080/customers/" +
+          customer._links.self.href.substring(32),
+        customer
+      )
+      .then((response) => {
+        console.log(response.data);
+        dispatch(updateCUSTOMERSuccess(response.data));
+      })
+      .catch((error) => dispatch(updateCUSTOMERFailure(error)));
+  };
 }
 
 export function deleteCUSTOMER(customerId) {
-  return dispatch => {
+  return (dispatch) => {
     dispatch(deleteCUSTOMERBegin());
     axios
-    .delete("http://localhost:8080/customers/" + customerId)
-    .then(response => {
-      console.log(response.data);
-      dispatch(deleteCUSTOMERSuccess(response.data));
-    })
-    .catch(error => dispatch(deleteCUSTOMERFailure(error)));
-  }
+      .delete("http://localhost:8080/customers/" + customerId)
+      .then((response) => {
+        console.log(response.data);
+        dispatch(deleteCUSTOMERSuccess(response.data));
+      })
+      .catch((error) => dispatch(deleteCUSTOMERFailure(error)));
+  };
 }
 
 export function fetchINTERACTION(customerId) {
-  return dispatch => {
+  return (dispatch) => {
     dispatch(fetchINTERACTIONsBegin());
     axios
-    .get("http://localhost:8080/interactions/search/findByCustomerId?id="+ customerId)
-    .then(response => {
-      console.log(response.data);
-      dispatch(fetchINTERACTIONsSuccess(response.data._embedded.interactions));
-    })
-    .catch(error => dispatch(fetchINTERACTIONsFailure(error)));
-  }
+      .get(
+        "http://localhost:8080/interactions/search/findByCustomerId?id=" +
+          customerId
+      )
+      .then((response) => {
+        console.log(response.data);
+        dispatch(
+          fetchINTERACTIONsSuccess(response.data._embedded.interactions)
+        );
+      })
+      .catch((error) => dispatch(fetchINTERACTIONsFailure(error)));
+  };
 }
 
 export function getINTERACTION(id) {
-  return dispatch => {
+  return (dispatch) => {
     dispatch(getINTERACTIONBegin());
     axios
-    .get("http://localhost:8080/interactions/" + id)
-    .then(response => {
-      console.log(response.data);
-      dispatch(getINTERACTIONSuccess(response.data));
-    })
-    .catch(error => dispatch(getINTERACTIONFailure(error)));
-  }
+      .get("http://localhost:8080/interactions/" + id)
+      .then((response) => {
+        console.log(response.data);
+        dispatch(getINTERACTIONSuccess(response.data));
+      })
+      .catch((error) => dispatch(getINTERACTIONFailure(error)));
+  };
 }
 
 export function deleteINTERACTION(id) {
-  return dispatch => {
+  return (dispatch) => {
     dispatch(deleteINTERACTIONBegin());
     axios
-    .delete("http://localhost:8080/interactions/" + id)
-    .then(response => {
-      console.log(response.data);
-      dispatch(deleteINTERACTIONSuccess(response.data));
-    })
-    .catch(error => dispatch(deleteINTERACTIONFailure(error)));
-  }
+      .delete("http://localhost:8080/interactions/" + id)
+      .then((response) => {
+        console.log(response.data);
+        dispatch(deleteINTERACTIONSuccess(response.data));
+      })
+      .catch((error) => dispatch(deleteINTERACTIONFailure(error)));
+  };
 }
 
 export function updateINTERACTION(interaction) {
-  return dispatch => {
+  return (dispatch) => {
     dispatch(updateINTERACTIONBegin());
     axios
-    .put("http://localhost:8080/interactions/" + interaction._links.self.href.substring(35), interaction)
-    .then(response => {
-      console.log(response.data);
-      dispatch(updateINTERACTIONSuccess(response.data));
-    })
-    .catch(error => dispatch(updateINTERACTIONFailure(error)));
-  }
+      .put(
+        "http://localhost:8080/interactions/" +
+          interaction._links.self.href.substring(35),
+        interaction
+      )
+      .then((response) => {
+        console.log(response.data);
+        dispatch(updateINTERACTIONSuccess(response.data));
+      })
+      .catch((error) => dispatch(updateINTERACTIONFailure(error)));
+  };
 }
 
 export function addINTERACTION(interaction) {
-  return dispatch => {
+  return (dispatch) => {
     dispatch(addINTERACTIONBegin());
     axios
-    .post("http://localhost:8080/interactions", interaction)
-    .then(response => {
-      console.log(response.data);
-      dispatch(addINTERACTIONSuccess(response.data));
-    })
-    .catch(error => dispatch(addINTERACTIONFailure(error)));
-  }
+      .post("http://localhost:8080/interactions", interaction)
+      .then((response) => {
+        console.log(response.data);
+        dispatch(addINTERACTIONSuccess(response.data));
+      })
+      .catch((error) => dispatch(addINTERACTIONFailure(error)));
+  };
 }
 
 const initialState = {
   customers: [],
   customer: {},
   interactions: [],
-  interaction: {}
-}
+  interaction: {},
+};
 
-export const store = createStore(customersReducer, initialState, composeWithDevTools(
-  applyMiddleware(thunk),
-  // other store enhancers if any
-));
-
-
+export const store = createStore(
+  customersReducer,
+  initialState,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+    // other store enhancers if any
+  )
+);
